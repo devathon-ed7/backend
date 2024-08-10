58c9cde1c631252f5101bee80debb2c5d4a0143f
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-08-09 18:56:04 -0600

    feature: Create products model

---
+++ models/mariadb/products.ts
@@ -0,0 +1,46 @@
+import { PrismaClient, Product } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export type CreateProductType = Omit<
+  Product,
+  "id" | "created_at" | "updated_at" | "user" | "supplier" | "category"
+>
+export type UpdateProductType = Partial<Product>
+
+export interface ProductModelInterface {
+  getAll: () => Promise<Product[]>
+  getById: (id: number) => Promise<Product>
+  getByIdWithRelations: (id: number) => Promise<Product>
+  getAllWithRelations: () => Promise<Product>
+  create: (data: CreateProductType) => Promise<Product>
+  update: (data: UpdateProductType) => Promise<Product>
+  delete: (id: number) => Promise<Product>
+}
+
+export default class ProductModel {
+  static getAll = async () => await prisma.product.findMany()
+  static getById = async (id: number) =>
+    await prisma.product.findUnique({ where: { id } })
+  static delete = async (id: number) =>
+    await prisma.product.delete({ where: { id } })
+  static create = async (data: CreateProductType) =>
+    await prisma.product.create({ data })
+  static udapte = async (data: UpdateProductType) =>
+    await prisma.product.update({ where: { id: data.id }, data })
+  static getAllWithRelations = async () =>
+    await prisma.product.findMany({
+      include: {
+        category: true,
+        supplier: true
+      }
+    })
+  static getByIdWithRelations = async (id: number) =>
+    await prisma.product.findUnique({
+      where: { id },
+      include: {
+        supplier: true,
+        category: true
+      }
+    })
+}
