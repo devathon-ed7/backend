4b89248d85aa8fb76ff9e36ace554d371743719b
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-05-13 22:17:43 -0500

    chore: map parentcategoryid and create category schemas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 //routers
 import { AuthRouter } from "./auth-router";
-import { CategoryRoutes } from "./category";
+import { CategoryRoutes } from "./category-router";
 import { ProductRouter } from "./products";
 import { SupplierRouter } from "./supplier";
 import { TransactionRouter } from "./transaction";
@@ -13,20 +13,28 @@ import ProductModel from "../models/products";
 import TransactionModel from "../models/transaction";
 import middleware from "../utils/middleware";
 
-
-
 export const registerRoutes = (app: Express, API_VERSION: string) => {
-
-
   app.use(`${API_VERSION}/users`, middleware.userExtractor, UserRouter());
   app.use(`${API_VERSION}/auth`, AuthRouter());
-  app.use(`${API_VERSION}/suppliers`, SupplierRouter({ supplierModel: SupplierModel }));
-  app.use(`${API_VERSION}/categories`, CategoryRoutes({ categoryModel: CategoryModel }));
+  app.use(
+    `${API_VERSION}/suppliers`,
+    SupplierRouter({ supplierModel: SupplierModel })
+  );
+  app.use(
+    `${API_VERSION}/categories`,
+    CategoryRoutes({ categoryModel: CategoryModel })
+  );
 
-  app.use(`${API_VERSION}/products`, ProductRouter({
-    categoryModel: CategoryModel,
-    supplierModel: SupplierModel,
-    productModel: ProductModel
-  }));
-  app.use(`${API_VERSION}/transactions`, TransactionRouter({ transactionModel: TransactionModel }));
+  app.use(
+    `${API_VERSION}/products`,
+    ProductRouter({
+      categoryModel: CategoryModel,
+      supplierModel: SupplierModel,
+      productModel: ProductModel
+    })
+  );
+  app.use(
+    `${API_VERSION}/transactions`,
+    TransactionRouter({ transactionModel: TransactionModel })
+  );
 };
---
+++ src/schemas/category-schema.ts
@@ -0,0 +1,54 @@
+import Joi from "joi";
+
+const id = Joi.string().optional();
+const name = Joi.string().required().max(255).messages({
+  "string.empty": "Name is not allowed to be empty",
+  "any.required": "Name is required"
+});
+const description = Joi.string().optional().allow(null, "").messages({
+  "string.base": "Description is not allowed to be empty"
+});
+const parentCategoryId = Joi.string().optional().allow(null);
+const created_at = Joi.date().optional();
+const updated_at = Joi.date().optional();
+
+const baseCategorySchema = Joi.object({
+  id,
+  name,
+  description,
+  parentCategoryId,
+  created_at,
+  updated_at
+});
+
+const createSchema = baseCategorySchema.fork(["name"], (schema) =>
+  schema.required()
+);
+const updateSchema = baseCategorySchema.keys({
+  id: id.required()
+});
+
+//operation schema
+
+const createCategorySchema = Joi.object({
+  data: createSchema
+});
+
+const updateCategorySchema = Joi.object({
+  data: updateSchema
+});
+
+const getCategorySchema = Joi.object({
+  id: id.required()
+});
+
+const deleteCategorySchema = getCategorySchema;
+
+export const categorySchemas = {
+  create: createCategorySchema,
+  update: updateCategorySchema,
+  get: getCategorySchema,
+  delete: deleteCategorySchema
+};
+
+export default categorySchemas;
